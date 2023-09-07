3d60f4613c3f0f3e3d9ee5ee3c7a3eae30d33b6b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-08 00:58:20 +0300

    4. Who are you? (A.C)

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__ =="__main__":
+if __name__ == "__main__":
     import hidden_4
-    
-    arr =  dir(hidden_4)  
+
+    arr = dir(hidden_4)
     for i in arr:
         if i[0:1] != "_":
             print(i)
