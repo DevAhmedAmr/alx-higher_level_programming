6167c4cf01e58783006788e906cb85abaec5c5e9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-08 00:26:21 +0300

    applied betty code style to 3. Infinite addition (A.C)

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    len=len(sys.argv)
-    result=0
-    for i in range(1,len):
-        result+=int(sys.argv[i])
+    len = len(sys.argv)
+    result = 0
+    for i in range(1, len):
+        result += int(sys.argv[i])
     print(result)
-        
