551a05beac41ea17a8778e966cf9e1fe0ecb948c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-09 19:08:59 +0300

    4. Replace in a copy (A.C)

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    
+    if my_list== None:
+        return
+    list_copy = my_list.copy()
+    
+    if idx < len(my_list):
+        list_copy[idx]=element
+        
+        return list_copy
+    
