b2085cd3a1830916683b0764d99a44b7d4f70aa4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-09 18:15:51 +0300

    2. Replace element (A.C)

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,16 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if (idx < 0) or (idx >= len(my_list)):
         return my_list
-    
-    if(idx==None):
-        return my_list
-    
-    if(my_list==None):
-        return None
-    
-    if element ==None:
-        return my_list
-    
+
     for i in range(len(my_list)):
         if (i == idx):
             my_list[i] = element
