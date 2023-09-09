145ebff644b17ee59761d02c0d6676fffbb249e1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-09 20:02:10 +0300

    5. Can you C me now? (A.C)

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if not my_string:
-        return None
+        return 
     strCpy=""
     for i in range(len(my_string)):
         if (my_string[i]!="C" and my_string[i]!="c"):
