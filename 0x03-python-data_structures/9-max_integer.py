e016de995b966f625e774cac0f1b899fe3699547
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-10 18:31:43 +0300

    9. Find the max (A.C)

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+
+    max_integer = 0
+    my_list_len = len(my_list)
+
+    if my_list is None or my_list_len == 0:
+        return None
+
+    for i in range(my_list_len):
+
+        if my_list[i] > max_integer:
+            max_integer = my_list[i]
+
+    return max_integer
