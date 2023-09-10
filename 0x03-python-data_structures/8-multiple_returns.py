3997cb6640530ec2a0b5d9d1084f30680b4e10fc
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-10 18:23:59 +0300

    fixed an input as empty string (A.C)

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    
     if sentence is not None:
         str_len = len(sentence)
         
-    if sentence is None or str_len is 0:
-        return (0 , None)
+
 
     
     first_char = sentence[0]
+    
     return (str_len, first_char)
