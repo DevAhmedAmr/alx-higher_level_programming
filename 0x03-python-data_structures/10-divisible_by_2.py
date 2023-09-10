798b0fc1cdf68a3bed03410483321ea0d0a1f81b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-10 19:00:02 +0300

    10. Only by 2 (A.C)

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,14 +2,14 @@
 def divisible_by_2(my_list=[]):
     if my_list is None:
         return None
-    
-    arr_len=len(my_list)
-    is_divisible_by_2=[]
-    
+
+    arr_len = len(my_list)
+    is_divisible_by_2 = []
+
     for i in range(arr_len):
-        if my_list[i] % 2 == 0 :
+        if my_list[i] % 2 == 0:
             is_divisible_by_2.append(True)
         else:
             is_divisible_by_2.append(False)
-            
+
     return is_divisible_by_2
