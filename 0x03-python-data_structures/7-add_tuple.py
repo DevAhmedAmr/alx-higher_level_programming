1cb14795f1d8a21e759db375205ebd3c12ee51ac
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-10 17:26:22 +0300

    7. Tuples addition (A.C)

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    list_a = tuple_to_list(tuple_a)
+    list_b = tuple_to_list(tuple_b)
+
+    sum_num1 = list_a[0]+list_b[0]
+    sum_num2 = list_a[1]+list_b[1]
+
+    return (sum_num1, sum_num2)
+
+
+def tuple_to_list(tuple_a=()):
+
+    tuple_a_len = len(tuple_a)
+    list_a = []
+
+    for i in range(tuple_a_len):
+        if tuple_a_len == 1:
+            list_a.append(tuple_a[i])
+            list_a.append(0)
+
+        elif tuple_a_len == 2:
+            list_a.append(tuple_a[i])
+
+    if tuple_a_len == 0:
+        list_a.append(0)
+        list_a.append(0)
+
+    return list_a
+
+# print(add_tuple(tuple_a, (1, )))
+# print(add_tuple(tuple_a, ()))
