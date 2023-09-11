ed2ee954bac0d9e815d3ddf9f76ce5d7eb8b01f3
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-12 01:38:38 +0300

    trying in c python (A.C)

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,7 +8,7 @@ void print_python_list_info(PyObject *p)
 	size = Py_SIZE(p);
 	printf("[*] Size of the Python List = %d\n", size);
 
-	mem_allocated = (p)->allocated;
+	mem_allocated = ((PyListObject *)p)->allocated;
 	printf("[*] Allocated = %d\n", mem_allocated);
 
 	while (i < size)
