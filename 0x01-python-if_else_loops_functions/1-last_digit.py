7dd9901f9bd2c0f01827dc5fa8f97471afe616d8
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-12 19:38:16 +0300

    0. Positive anything is better than negative nothing (A.C)

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,4 @@ if (abs(number) % 10 > 5):
     print(" and is greater than 5",end="")
 elif (abs(number) % 10 < 6):
     print("and is less than 5",end="")
-    
-if abs(number) % 10 == 0:
-    print("and is 0")
-else:
-    print("and not 0")
 
