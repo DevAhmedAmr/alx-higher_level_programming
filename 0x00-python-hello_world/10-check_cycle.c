2d2c742b56b989f5cb5ee5f8358cebecf86eb8f4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-12 18:15:16 +0300

    test (A.C)

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -20,20 +20,7 @@ int check_cycle(listint_t *list)
 
 		if (list == p2)
 		{
-			list = prev;
-			prev = p2;
-			while (1)
-			{
-				p2 = prev;
-				while (p2->next != list && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == list)
-					break;
 
-				list = list->next;
-			}
 			return (1);
 		}
 	}
