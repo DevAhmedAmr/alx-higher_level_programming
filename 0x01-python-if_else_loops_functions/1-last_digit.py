54465da215f667bd9c40e27334fd4be3125c7600
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-12 19:37:42 +0300

    0. Positive anything is better than negative nothing (A.C)

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ print(f"Last digit of {number} is { abs(number) % 10}", end="")
 if (abs(number) % 10 > 5):
     print(" and is greater than 5",end="")
 elif (abs(number) % 10 < 6):
-    print("and is less than 5")
+    print("and is less than 5",end="")
     
 if abs(number) % 10 == 0:
-    print("and is 0",end="")
+    print("and is 0")
 else:
-    print("and not 0",end="")
+    print("and not 0")
 
