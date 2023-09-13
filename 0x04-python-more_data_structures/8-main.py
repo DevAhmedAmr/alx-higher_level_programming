1f5c78bdac353ec2388e3c5754b91bd2d1a54960
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-13 22:40:37 +0300

    test (A.C)

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -7,7 +7,7 @@ a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3]
 new_dict = simple_delete(a_dictionary, 'track')
 print_sorted_dictionary(a_dictionary)
 print("--")
-print(new_dict)
+print_sorted_dictionary(new_dict)
 
 print("--")
 print("--")
