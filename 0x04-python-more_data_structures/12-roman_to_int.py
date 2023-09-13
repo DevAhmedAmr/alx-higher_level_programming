8d252b1bc6cbd2f99c14a1113e072b4e30ad4efb
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 01:23:26 +0300

    Revert "Revert "12. Roman to Integer (A.C)""
    This reverts commit da2b8df043b2d8e09840f996047260ba36287a6a.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     number = 0
     prev = 0
     roman_dic = {"I": 1, "V": 5, "X": 10,
-                "L": 50, "C": 100, "D": 500, "M": 1000}
+                 "L": 50, "C": 100, "D": 500, "M": 1000}
 
     if type(roman_string) is not str or roman_string is None:
         return 0
