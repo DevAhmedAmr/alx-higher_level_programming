24e85780351e3d4332be86e400e43cde92ceffa5
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 00:49:51 +0300

    12. Roman to Integer (A.C)

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,6 +5,9 @@ def roman_to_int(roman_string):
     roman_dic = {"I": 1, "V": 5, "X": 10,
                 "L": 50, "C": 100, "D": 500, "M": 1000}
     
+    if not roman_string  or type(roman_string) != str:
+        return None
+    
     for i in  range(len(roman_string)):
         
             
