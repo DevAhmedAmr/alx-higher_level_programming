15f31d772f8e91a7e7779f2e671b1c7de75ce395
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-13 20:11:42 +0300

    test (A.C)

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -21,8 +21,5 @@ def square_matrix_simple2(matrix=[]):
 
 
 def square_matrix_simple(matrix=[]):
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     result = [[x * x for x in elm] for elm in matrix]
     return result
-
-
