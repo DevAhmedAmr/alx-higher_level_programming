af806611c679bc43be52bc4ffa7791ee06acd96a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-13 21:20:42 +0300

    1. Search and replace (A.C)

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_set = set()
-    
-    for element in set_1:
-        # Use bitwise AND to check if the element is also in set_2
-        if element in set_2:
-            common_set.add(element)
-    
-    return common_set
-
-# Example usage:
-set1 = {1, 2, 3, 4, 5}
-set2 = {3, 4, 5, 6, 7}
-
-result = common_elements(set1, set2)
-print(result)  # Output: {3, 4, 5}
+    common_elements = set_1 & set_2
+    return common_elements
