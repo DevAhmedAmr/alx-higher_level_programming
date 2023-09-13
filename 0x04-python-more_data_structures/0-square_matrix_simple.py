bc765d42a558b43e85e10e2bcd6f9e258b60c1e9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-13 19:38:23 +0300

    test (A.C)

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_sq=[]
-    
+    matrix_sq = []
+
     if matrix is None:
         return None
-    
+
     for i in range(len(matrix)):
-        
-        sub_matrix_sq=[]
-        
+
+        sub_matrix_sq = []
+
         if matrix[i] is None:
             matrix_sq.append(None)
             continue
 
-        
         for j in range(len(matrix[i])):
-            
             sub_matrix_sq.append(matrix[i][j]*matrix[i][j])
-            
         matrix_sq.append(sub_matrix_sq)
-        
+
     return matrix_sq
+
+
+def square_matrix_simple_lambda(matrix=[]):
+    matrix_sq = []
+    x = matrix[0]
+    for i in matrix:
+        result = map((lambda x: x*x), i)
+        matrix_sq.append(list(result))
+    return matrix_sq
+
+
+
