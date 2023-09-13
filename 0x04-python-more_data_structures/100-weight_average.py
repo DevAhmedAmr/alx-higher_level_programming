084f80d45f6b2528f96dc206bc7d7b8cab14357a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 01:43:34 +0300

    13. Weighted average! (A.C)

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    list_len = len(my_list)
+    total_avg=0
+    weight_avg=0
+    
+    for i in range(list_len):
+        multi = 1
+        
+        for j in range(len(my_list[i])):
+            multi *= my_list[i][j]
+            
+        total_avg +=multi
+        weight_avg +=my_list[i][1]
+        
+    return total_avg/weight_avg
