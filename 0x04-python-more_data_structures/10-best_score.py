278527fe788301e72276dfba3348d18a37c69bd3
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-13 23:02:01 +0300

    10. Best score (A.C)

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    biggest_integer_value = -999999999999999
+    biggest_integer_key= None
+    
+    if a_dictionary is None:
+        return None
+    
+    for i in a_dictionary:
+        if a_dictionary[i] > biggest_integer_value:
+            biggest_integer_value = a_dictionary[i]
+            biggest_integer_key = i
+            
+    return biggest_integer_key
