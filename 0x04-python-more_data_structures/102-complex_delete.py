922e272ad96bdf11cc6ea1d89ff4f9ca6e38daa8
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 03:15:36 +0300

    first commit (A.C)

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value): 
-    keys_to_be_deleted=[]
-    
+def complex_delete(a_dictionary, value):
+    keys_to_be_deleted = []
+
     if a_dictionary is None or value is None:
         return None
-    
+
     for i in a_dictionary:
         if a_dictionary[i] == value:
             keys_to_be_deleted.append(i)
-            
+
     if len(keys_to_be_deleted) == 0:
         return a_dictionary
-            
+
     for i in range(len(keys_to_be_deleted)):
         a_dictionary.pop(keys_to_be_deleted[i])
-        
-    return a_dictionary
 
+    return a_dictionary
