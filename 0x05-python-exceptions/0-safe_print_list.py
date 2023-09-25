5e865c1662b8b4db0ac1fd2b672c72314471c4c3
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-25 22:48:33 +0300

    first commit (A.C)

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -17,12 +17,3 @@ def safe_print_list(my_list=[], x=0):
         print("\n", end="")
 
     return counter
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 992)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
