59064b47d782076b3a2b4926a5fb85aa4d062d4d
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-25 22:45:42 +0300

    first commit (A.C)

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,7 @@ def safe_print_list(my_list=[], x=0):
 
             if i == x-1:
                 print("")
-        counter = counter+1
+            counter = counter+1
 
     except Exception:
         print("", end="")
