54dfe4160084d4071155e33ef22b2935d5b724d2
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-26 02:03:31 +0300

    3. Integers division with debug (A.C)

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a/b
+        
+    finally:
+        return result
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
