4ddba10e90946952296e0beed63156221c6dd9ce
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-26 02:14:26 +0300

    3. Integers division with debug (A.C)

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a/b
-        
+    except Exception :
+        pass
     finally:
         print(f"Inside result: {result}")
     return result
-
