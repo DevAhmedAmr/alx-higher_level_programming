ee302b56e14882f1649972babc03e664e03af85a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-25 22:45:15 +0300

    first commit (A.C)

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,13 +5,13 @@ def safe_print_list(my_list=[], x=0):
     if my_list is None:
         return 0
     try:
-        for counter in range(x):
+        for i in range(x):
 
-            print(my_list[counter], end="")
+            print(my_list[i], end="")
 
-            if counter == x-1:
+            if i == x-1:
                 print("")
-            counter = counter+1
+        counter = counter+1
 
     except Exception:
         print("", end="")
