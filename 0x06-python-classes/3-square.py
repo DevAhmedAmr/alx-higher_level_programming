f83fdfc4d5f2f17cb9c41e9b577933712c5bdd48
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-27 03:10:17 +0300

    2. Size validation (A.C)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,7 +14,6 @@ class Square:
 
         if self.__size < 0:
             raise ValueError("size must be >= 0")
-        
+
     def area(self):
-        return self.__size *self.__size
-        
+        return self.__size * self.__size
