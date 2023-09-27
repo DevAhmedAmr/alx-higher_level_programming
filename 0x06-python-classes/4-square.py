03b5d48858abb6eae2fbd305f5ea5a66165c8891
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-27 04:03:54 +0300

    2. Size validation (A.C)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+
+        self.__size = size
+
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return self.__size * self.__size
+    
+    @property
+    def size(self):
+        return self.__size
+
+
+    @size.setter
+    def size(self,size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size=size
+
