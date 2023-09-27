437857982ec300e1009d9c0f843e4973a6f1c014
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-27 05:17:51 +0300

    6. Coordinates of a square  (A.C)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,60 @@
 #!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0,position = (0,0)):
+        self.__size = size
+        self.position = position
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        
+    @property
+    def position(self):
+        """getter method"""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """position setter"""
+        if type(value) != tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif type(value[0]) and type(value[1]) != int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif value[0] and value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        else:
+            self.__position = value
+        
+    def my_print(self):
+        
+        if self.position[1] !=0:
+            for k in range(self.position[1]):
+                print("")
+            
+        for i in range(self.size):
+            for k in range(self.position[0]):
+                print(" ",end="")
+                
+            for j in range(self.size):
+                print("#", end="")
+            print("")
+
+
