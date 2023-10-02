667ae786f77a1195a2a96855e71d5b89ef280a79
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-02 21:12:51 +0300

    1. Real definition of a rectangle (A.C)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,36 +5,35 @@
 
 class Rectangle:
     """ Rectangle """
-    
-    def __init__(self, width=0,height=0):
+
+    def __init__(self, width=0, height=0):
         self.width = width
-        self.height=height
-        
-		
+        self.height = height
+
     @property
     def width(self):
         """ width """
         return self.__width
 
     @width.setter
-    def width(self,value):
+    def width(self, value):
         """ width """
-        if type(value)!= int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width=value
+        self.__width = value
 
     @property
     def height(self):
         """ height """
         return self.__height
-    
+
     @height.setter
-    def height(self,value):
+    def height(self, value):
         """ height """
-        if type(value)!= int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height=value
+        self.__height = value
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,1 +1,54 @@
 #!/usr/bin/python3
+
+"""_summary_"""
+
+
+class Rectangle:
+    """ Rectangle """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+        
+    def area(self):
+        return self.__height*self.__width
+    
+    def perimeter(self):
+        return (self.__height+self.__width)*2
+
+    if __name__=="__main__":
+        Rectangle = __import__('2-rectangle').Rectangle
+        my_rectangle = Rectangle(2, 4)
+        print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+        print("--")
+        my_rectangle.width = 10
+        my_rectangle.height = 3
+        print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
