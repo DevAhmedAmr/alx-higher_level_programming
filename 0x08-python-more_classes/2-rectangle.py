dbb1db75f7d14afae26b8f44e910b51ad4335ecb
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-02 21:12:56 +0300

    1. Real definition of a rectangle (A.C)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,11 +44,3 @@ class Rectangle:
     def perimeter(self):
         return (self.__height+self.__width)*2
 
-    if __name__=="__main__":
-        Rectangle = __import__('2-rectangle').Rectangle
-        my_rectangle = Rectangle(2, 4)
-        print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-        print("--")
-        my_rectangle.width = 10
-        my_rectangle.height = 3
-        print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
