4cac3a54ea731f52deb4c68a054f3d5434d0c6a0
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-02 21:41:40 +0300

    4. Eval is magic  (A.C)

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ width """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ height """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -59,8 +59,7 @@ class Rectangle:
 
             if i != self.height-1:
                 str += "\n"
-
         return str
-    
+
     def __repr__(self):
         return f"Rectangle({self.width:d}, {self.height})"
