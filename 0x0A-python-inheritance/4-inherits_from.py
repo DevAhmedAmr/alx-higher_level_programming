16111d40fa292a9f1303d6669598ecf8ade46667
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 01:02:02 +0300

    first commit (A.C)

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""is_kind_of_class """
+
+
+def inherits_from(obj, a_class):
+    """is_kind_of_class """
+    return False if type(obj) is a_class else isinstance(obj, a_class)
