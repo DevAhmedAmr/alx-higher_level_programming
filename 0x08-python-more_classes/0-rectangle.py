523e46c627ac3c9983191f8279819765df07192c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-09 22:30:57 +0300

    first commit (A.C)

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-"""_summary_
-"""
+"""_summary_"""
 
 
 class Rectangle:
--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+import math
+
+n = int(1e100)
+factorial = math.factorial(n)
+print(factorial)
