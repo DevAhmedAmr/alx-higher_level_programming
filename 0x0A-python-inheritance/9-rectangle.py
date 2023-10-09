70ae40c616b1722c7d5db79a52d77924c7693601
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 02:56:52 +0300

    9. Full rectangle (A.C)

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""empty class  """
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
+    """empty class  """
 
     def __init__(self, width, height):
-        """Method for initialized the attrubutes"""
-        self.integer_validator("width", width)
-        self.__width = width
+        """__init__   """
+
         self.integer_validator("height", height)
+        self.integer_validator("width", width)
+
+        self.__width = width
         self.__height = height
-
+        
     def area(self):
-        """Method to redefine a area method in the parent class"""
-
+        """area"""
         return self.__width * self.__height
-
+    
     def __str__(self):
-        """__str__ method for return the next string"""
-
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        """__str__"""
+        return "[Rectangle] {}/{}".format(self.__width,self.__height)
