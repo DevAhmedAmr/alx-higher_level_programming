fe0a4f1c6b3f3ada2b96c570e3cce9e2eeb763bb
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 02:20:52 +0300

    first commit (A.C)

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,6 +6,22 @@ class Rectangle(BaseGeometry):
     """empty class  """
     def __init__(self, width, height):
         
-        self.integer_validator(width,height)
+        self.integer_validator("height",height)
+        self.integer_validator("width",width)
+
         self.__width=width
         self.__height=height
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
+
+try:
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
