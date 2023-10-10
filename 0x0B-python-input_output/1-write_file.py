ca42f951de42df37fcc9a2d61505bbbdcafc8c42
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 20:23:44 +0300

    0. Read file (A.C)

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """write_file
+            Args:
+            filename (str, optional): the file name to be opened. Defaults to "".
+            text (str, optional): text to be written to. Defaults to "". """
+    with open(filename, "w+") as file:
+        return file.write(text)
+
