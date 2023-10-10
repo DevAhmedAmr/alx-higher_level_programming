119bd26c7eb2ff2be615f11a36cd3a60f91f7911
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 03:33:35 +0300

    9. Full rectangle (A.C)

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""old _ empty class  """
-Rectangle  = __import__("9-rectangle").Rectangle 
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
 
 
 class Square(Rectangle):
-    """Square"""
+    """Square class that inherits from Rectangle that inherits BaseGeometry"""
 
     def __init__(self, size):
-        """__init__"""
-        super().__init__(size, size)        
+        """Method for initialized the attrubutes"""
+
+        super().__init__(size, size)
         self.integer_validator("size", size)
-        self.__size=size
-        
+        self.__size = size
+
     def area(self):
-        """area"""
-        return self.__size * 2
+        """rectangle area"""
+
+        return self.__size ** 2
