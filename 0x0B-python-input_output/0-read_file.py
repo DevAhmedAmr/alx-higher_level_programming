07445419cb53997cf08b901857a210a9c680dfc0
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 20:08:43 +0300

    0. Read file (A.C)

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 "function reads from a file"
+
+
 def read_file(filename=""):
     "function reads from a file"
-    with open(filename,"r",encoding="UTF8") as file:
-            for line in file :
-                print(line,end="")
-
+    with open(filename, "r", encoding="UTF8") as file:
+        for line in file:
+            print(line, end="")
