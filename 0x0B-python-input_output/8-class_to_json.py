7c1623fa4e954ee025f381186a1ec9e494260279
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 22:54:40 +0300

    8. Class to JSON (A.C)

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""json module"""
+import json
+
+
+def class_to_json(obj):
+    """_summary_
+
+    Args:
+            obj (_type_): _description_
+
+    Returns:
+            _type_: _description_
+    """
+    my_dictionary = dict()
+    my_dictionary["name"] = obj.name
+    my_dictionary["number"] = obj.number
+    return json.dumps(my_dictionary)
