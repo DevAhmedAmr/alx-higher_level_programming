fe5ff77e83bc2d36175035e6c0154b573c2ad522
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-11 02:17:27 +0300

    10. Student to JSON with filter (A.C)

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+def pascal_triangle(n):
+    """ pascal triangle """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
+
+# def pascal_triangle(n):
+#     list_a =[]
+#     for i in range(1,n+1):
+#         list_b=[]
+#         for j in range(i):
+#             if i <=2:
+#                 list_b.append(1)
+
+#         list_a.append(list_b)
+
+
+# def print_triangle(triangle):
+#     """
+#     Print the triangle
+#     """
+#     for row in triangle:
+#         print("[{}]".format(",".join([str(x) for x in row])))
+
+
+# if __name__ == "__main__":
+#     print_triangle(pascal_triangle(5))
