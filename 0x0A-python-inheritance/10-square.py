f9d01b852d652c9e6a7d2793e8afa2f27a3ea398
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 03:28:18 +0300

    9. Full rectangle (A.C)

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,9 +2,13 @@
 """old _ empty class  """
 Rectangle  = __import__("9-rectangle").Rectangle 
 
+
 class Square(Rectangle):
+    """Square"""
+
     def __init__(self, size):
         """__init__"""
+        super().__init__(size, size)        
         self.integer_validator("size", size)
         self.__size=size
         
