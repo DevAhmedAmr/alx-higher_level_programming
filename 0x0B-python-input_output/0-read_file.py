1db81a09122c4eb0f64b625239cde650cd59e3ae
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 20:06:09 +0300

    0. Read file (A.C)

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"function reads from a file"
+def read_file(filename=""):
+    "function reads from a file"
+    
+    try:
+        with open(filename,"r",encoding="UTF8") as file:
+            for line in file :
+                print(line,end="")
+    except FileNotFoundError:
+        print("file doesn't exist")
