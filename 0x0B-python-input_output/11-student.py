dfada1fd087fd8111f1e3fef799623ac3c96f3e6
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-11 01:57:48 +0300

    10. Student to JSON with filter (A.C)

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""_summary_
+Returns:
+_type_: _description_"""
+
+
+class Student:
+    """_summary_
+        """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """_summary_"""
+        dictionary = {}
+        if attrs is None:
+            return vars(self)
+        for i in attrs:
+            if i in vars(self).keys():
+                dictionary[i] = vars(self)[i]
+                
+    def reload_from_json(self, json):
+        """_summary_
+
+		Args:
+			json (_type_): _description_
+		"""
+        if json.has_key("first_name"):
+            self.first_name = json["first_name"]
+            
+        if json.has_key("last_name"):
+            self.last_name= json["last_name"]
+            
+        if json.has_key("age"):
+            self.age=json["age"]
+        
