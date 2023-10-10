33432ae4509b67160a166ff22fc49c50620bfacf
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 20:53:48 +0300

    2. Append to a file (A.C)

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import json
+"""
+function that returns the JSON representation of an  object (string):
+"""
+
+
+def to_json_string(my_obj):
+    """_summary_
+
+    Args:
+            my_obj (_type_): _description_
+    """
+    return json.dumps(my_obj)
