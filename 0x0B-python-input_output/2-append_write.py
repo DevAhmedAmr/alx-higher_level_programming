f54f0a7818ddbf0f5e66b373aca09f9801fa000d
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-10 20:38:57 +0300

    2. Append to a file (A.C)

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""function that appends a string at the end of a text file """
+
+
 def append_write(filename="", text=""):
+    """_summary_
+
+    Args:
+            filename (str, optional): _description_. Defaults to "".
+            text (str, optional): _description_. Defaults to "".
+
+    Returns:
+            _type_: _description_
+    """
     with open(filename, "a") as file:
         return file.write(text)
-    
-nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
-print(nb_characters_added)
