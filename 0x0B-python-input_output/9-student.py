c51aa35512eff86a1158c3b10166196e2941bd43
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-11 01:22:58 +0300

    8. Class to JSON (A.C)

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,13 +2,16 @@
 """_summary_
 Returns:
 _type_: _description_"""
+
+
 class Student:
     """_summary_
-	"""
+        """
+
     def __init__(self, first_name, last_name, age):
-        self.first_name=first_name
-        self.last_name=last_name
-        self.age=age
-    
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
     def to_json(self):
         return vars(self)
