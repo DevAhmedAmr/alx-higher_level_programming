18df498306d3c3a5fde36d6329b5067013b6c1bd
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-16 19:33:19 +0300

    first commit (A.C)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-Base = __import__("base").Base
+""" Module that contains class Rectangle,
+inheritance of class Base
+"""
+from models.base import Base
+
 
 
 class Rectangle(Base):
