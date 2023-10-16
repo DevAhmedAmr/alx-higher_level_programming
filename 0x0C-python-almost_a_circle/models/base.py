61571d149b07004f06013095d6689b28e32da7e9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-16 19:15:20 +0300

    first commit (A.C)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,3 +40,12 @@ class Base:
             return []
         return json.loads(json_string)
     
+    @classmethod
+    def create(cls, **dictionary): 
+        """create"""
+        if cls.__name__ == "Rectangle":
+            new_instance = cls(10,10)
+        else:
+            new_instance = cls(10)
+        new_instance.update(**dictionary)   
+        return  new_instance
