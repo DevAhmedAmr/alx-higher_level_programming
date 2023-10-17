c23073c5a31b48d4eaed73ef9244e57c755280d3
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-17 03:59:01 +0300

    first commit (A.C)

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -30,10 +30,52 @@ class testRectangle(unittest.TestCase):
         
     def test_Rec_constructor4(self):
         """test_Rec_constructor4"""
-        rec = Rectangle(1, 10,8,7)
+        rec = Rectangle(1, 10,8,7,12)
         rec.create(**{"id":12,"width":18})
         self.assertEqual(rec.x, 8)
         self.assertEqual(rec.y, 7)
+        self.assertEqual(rec.id, 12)
 
+        
+    def test_Rec_constructor5(self):
+        """test_Rec_constructor5"""
+        rec = Rectangle(1, 10,8,7)
+        rec.update(1,2,3,4,5)
+        self.assertEqual(rec.x, 4)
+        self.assertEqual(rec.y, 5)
+        self.assertEqual(rec.id, 1)
+        self.assertEqual(rec.y, 5)
+        
+    def test_Rec_constructor6(self):
+        """test_Rec_constructor6"""
+
+        rec = Rectangle(1, 1,1,1,8)
+        self.assertEqual(rec.id, 8)
+        rec.update(1,2,3,4,5)
+        self.assertEqual(rec.id, 1)
+        self.assertEqual(rec.width, 2)
+        self.assertEqual(rec.height, 3)
+        self.assertEqual(rec.x, 4)
+        self.assertEqual(rec.y, 5)
+        
+    def test_Rec_constructor7(self):
+        """test_Rec_constructor6"""
+        rec = Rectangle(1, 10,8,7)
+        rec.update(**{"id": 1,"width":2,"height":3,"x":4,"y":5})
+        self.assertEqual(rec.id, 1)
+        self.assertEqual(rec.width, 2)
+        self.assertEqual(rec.height, 3)
+        self.assertEqual(rec.x, 4)
+        self.assertEqual(rec.y, 5)
+        
+    def test_Rec_constructor7(self):
+        """test_Rec_constructor6"""
+        rec = Rectangle(1, 10,8,7)
+        rec.update(10,20,30,40,50,**{"id": 1,"width":2,"height":3,"x":4,"y":5})
+        self.assertEqual(rec.id, 1)
+        self.assertEqual(rec.width, 2)
+        self.assertEqual(rec.height, 3)
+        self.assertEqual(rec.x, 4)
+        self.assertEqual(rec.y, 5)
 if __name__ == '__main__':
     unittest.main()
