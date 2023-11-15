6d622a5a69b5bc9fea2bceb643906b29c5cf3198
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-15 02:43:46 +0200

    9. Full creation (automated command)

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -4,5 +4,5 @@
 -- 		*Results should display both the score and the name (in this order)
 -- 		*Records should be ordered by score (top first)
 -- 		*The database name will be passed as an argument of the mysql command
-select `second_table`  from hbtn_0c_0.tables 
+SHOW `second_table`  from hbtn_0c_0.tables 
 where `second_table`  = 'hbtn_0c_0' order by score;
