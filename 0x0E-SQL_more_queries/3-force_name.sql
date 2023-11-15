2c89062a7b7aed2b2d68b439066eea6cf281c5b2
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-16 01:13:16 +0200

    4. ID can't be null (automated command)

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -6,5 +6,4 @@
 -- 	*The database name will be passed as an argument of the mysql command
 -- 	*If the table force_name already exists, your script should not fail
 
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256)  NOT NULL) ;
-
+CREATE TABLE IF NOT EXISTS id_not_null  (id INT, name VARCHAR(256)  NOT NULL) ;
