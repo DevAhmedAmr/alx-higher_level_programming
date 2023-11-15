5676413a10ccdabd28fd0b09ef5242c7f5d9ad60
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-15 02:22:07 +0200

    8. Count 89  (automated command)

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -2,5 +2,6 @@
 -- in the table first_table of the database hbtn_0c_0 in your MySQL server.
 
 -- The database name will be passed as an argument of the mysql command
-SELECT count( * ) as total_record FROM `first_table` 
+SELECT count( * ) as total_record 
+FROM `first_table` 
 WHERE id = 89;
