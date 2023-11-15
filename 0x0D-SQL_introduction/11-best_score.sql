b9b57c8a9a2d308b62ede291adc00d33ed693a2c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-15 03:00:36 +0200

    10. List by best (automated command)

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -6,4 +6,5 @@
 -- 		The database name will be passed as an argument of the mysql command
 SELECT `score`, `name`
 FROM `second_table`
-WHERE score >= 10 DESC
+WHERE score >= 10
+ORDER BY score DESC
