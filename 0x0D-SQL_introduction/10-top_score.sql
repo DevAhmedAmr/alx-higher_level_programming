da1323e143a1fa017bb103680d8fa586e538ba20
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-15 02:46:40 +0200

    10. List by best (automated command)

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -4,5 +4,6 @@
 -- 		*Results should display both the score and the name (in this order)
 -- 		*Records should be ordered by score (top first)
 -- 		*The database name will be passed as an argument of the mysql command
-SHOW `second_table`  from hbtn_0c_0.tables 
-where `second_table`  = 'hbtn_0c_0' order by score;
+SELECT *
+FROM `second_table`
+ORDER BY `score` DESC;
