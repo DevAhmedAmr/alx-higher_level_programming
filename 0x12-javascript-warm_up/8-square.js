979cccc8741b11a63de6e4da56940d99da4f0ff6
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-11 21:57:37 +0200

    8. Square (automated command)

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+let len = parseInt(process.argv[2]);
+
+if (isNaN(len)) {
+    console.log("Missing size");
+    return;
+}
+for (let i = 0; i < len; i++) {
+    let buffer = "";
+
+    for (let j = 0; j < len; j++) buffer += "X";
+
+    console.log(buffer);
+}
