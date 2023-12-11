9a6a90dde334a5c0d18291d7e1ad4cea61912d1f
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-11 21:48:02 +0200

    7. I love C (automated command)

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+let x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+    console.log("Missing number of occurrences");
+    return;
+}
+for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+}
