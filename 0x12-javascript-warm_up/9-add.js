7f7936c99058403470c99832a586b6807c0086c8
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-11 22:01:28 +0200

    9. Add (automated command)

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let num1 = parseInt(process.argv[2]);
-let num2 = parseInt(process.argv[2]);
+let num2 = parseInt(process.argv[3]);
 
 if (isNaN(num1) || isNaN(num2)) {
     console.log("NaN");
