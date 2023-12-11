f2870a506364616af6e035fe458d20f7908b7967
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-11 21:59:48 +0200

    8. Square (automated command)

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+let num1 = parseInt(process.argv[2]);
+let num2 = parseInt(process.argv[2]);
+
+if (isNaN(num1) || isNaN(num2)) {
+    console.log("NaN");
+    return;
+}
+
+console.log(num1 + num2);
