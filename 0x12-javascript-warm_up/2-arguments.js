1b2af4bf3a595a13cd6f7eb374f492128fd42362
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-11 19:59:25 +0200

    2. Arguments (automated command)

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+if (process.argv[2] === undefined) {
+	console.log('No argument');
+	return;
+}
+if (process.argv.length === 3) {
+	console.log('Argument found');
+	return;
+}
+console.log('Arguments found');
