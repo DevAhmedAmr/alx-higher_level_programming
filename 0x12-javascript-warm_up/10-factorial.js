cf45ee002fdefc3ba303a508bf7c7bcecef1467f
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-11 22:13:26 +0200

    10. Factorial (automated command)

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+function factorial(n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+let arg = parseInt(process.argv[2]);
+
+if (isNaN(arg)) arg = 1;
+
+console.log(factorial(arg));
