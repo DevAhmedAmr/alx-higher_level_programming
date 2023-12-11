ebb9eac824cabee5c534f08f810784295684cee6
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-11 22:29:43 +0200

    11. Second biggest! (automated command)

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+let arr = [];
+
+for (let i = 2; i < process.argv.length; i++) {
+    arr.push(parseInt(process.argv[i]));
+}
+
+let max_number = arr[0];
+let second_maxNumber = 0;
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max_number) {
+        second_maxNumber = max_number;
+        max_number = arr[i];
+    }
+
+    if (arr[i] > second_maxNumber && arr[i] < max_number)
+        second_maxNumber = arr[i];
+}
+console.log(second_maxNumber);
