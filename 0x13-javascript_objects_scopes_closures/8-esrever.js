2e3ce0a8ff9dceb4a0546c3d5f5452d3dccef333
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-12 23:03:27 +0200

    8. Esrever (automated command)

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const arr = [];
+  for (let i = 0; i < list.length; i++) {
+    const j = list.length - 1 - i;
+    arr.push(list[j]);
+  }
+  return arr;
+};
