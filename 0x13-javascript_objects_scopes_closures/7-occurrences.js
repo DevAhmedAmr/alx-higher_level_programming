9238508d5e5a82f0127e877b5694f57ca11f8260
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-12 22:57:24 +0200

    7. Occurrences (automated command)

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let occurrencesNum = 0;
+  for (const i of list) {
+    if (i === searchElement) occurrencesNum++;
+  }
+  return occurrencesNum;
+};
