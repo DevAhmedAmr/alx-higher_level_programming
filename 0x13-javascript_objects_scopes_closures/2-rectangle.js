0883919fe95b12644842858b0c1d5f0295678757
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-12 22:06:12 +0200

    2. Rectangle #2 (automated command)

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
-  constructor(w, h) {
-    this.width = w;
-    this.height = h;
-    if (condition) {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
