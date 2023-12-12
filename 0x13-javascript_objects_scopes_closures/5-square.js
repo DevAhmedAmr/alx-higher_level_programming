44c21294af726481321538ea18379cb2e62fad89
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-12 22:25:44 +0200

    task5: Square (automated command)

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
-  constructor (size) {
-    super();
-    if (size > 1) {
-      this.height = size;
-      this.width = size;
-    }
+  constructor(size) {
+    super(size, size);
   }
 }
 
