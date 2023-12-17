52c0f2482099e30fdaba02e2c3dc807df0c582d8
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-17 22:37:21 +0200

    3. SQL Injection... (automated command)

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,17 +2,10 @@
 """blank"""
 import MySQLdb
 import sys
+"""blank"""
 
 if __name__ == "__main__":
-    """blank"""
-    def remove_special_characters(string):
-        """blank"""
-        special_characters = set("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
-        new_str = ''
-        for char in string:
-            if char not in special_characters:
-                new_str += char
-        return new_str
+
 
     user = sys.argv[1]
     password = sys.argv[2]
@@ -21,8 +14,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=user,
                          password=password, database=database)
     cur = db.cursor()
+    # "SELECT * FROM products WHERE prod_name LIKE '%A'"
     cur.execute(
-        """SELECT * FROM `states` WHERE BINARY `name` = '{}'""".format(remove_special_characters(state_name)))
+       """SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'""".format(state_name))
 
     data = cur.fetchall()
 
@@ -30,3 +26,4 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
+    
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""blank"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """blank"""
+    def remove_special_characters(string):
+        """blank"""
+        special_characters = set("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+        new_str = ''
+        for char in string:
+            if char not in special_characters:
+                new_str += char
+        return new_str
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         password=password, database=database)
+    cur = db.cursor()
+    print(remove_special_characters(state_name))
+    cur.execute(
+        """SELECT * FROM `states` WHERE BINARY `name` = '{}'""".format(remove_special_characters(state_name)))
+
+    data = cur.fetchall()
+
+    for row in data:
+        print(row)
+    cur.close()
+    db.close()
