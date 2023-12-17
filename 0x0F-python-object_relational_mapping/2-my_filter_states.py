3f3d8294f1ba5d1805e9ce7152f7538a200154df
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-17 21:38:01 +0200

    2. Filter states by user input (automated command)

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""blank"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name=sys.argv[4]
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         password=password, database=database)
+    cur = db.cursor()
+    # "SELECT * FROM products WHERE prod_name LIKE '%A'"
+    cur.execute(
+          f"""SELECT * FROM states WHERE states.name='{state_name}';""")
+
+    data = cur.fetchall()
+    
+    for row in data:
+        print(row)
+    cur.close()
+    db.close()
