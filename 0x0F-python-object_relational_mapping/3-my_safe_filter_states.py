9535e3dbf0c57cd0edc41203fe7404d3c3819ad7
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-17 22:51:22 +0200

    3. SQL Injection... (automated command)

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,17 +2,12 @@
 """blank"""
 import MySQLdb
 import sys
-
+import re
 if __name__ == "__main__":
     """blank"""
-    def remove_special_characters(string):
-        """blank"""
-        special_characters = set("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
-        new_str = ''
-        for char in string:
-            if char not in special_characters:
-                new_str += char
-        return new_str
+    def sanitize_input(input_string):
+        sanitized_string = re.sub(r'[^a-zA-Z0-9\s]', '', input_string)
+        return sanitized_string
 
     user = sys.argv[1]
     password = sys.argv[2]
@@ -21,8 +16,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=user,
                          password=password, database=database)
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (state_name,))
+    cur.execute(
+        """SELECT * FROM `states` WHERE BINARY `name` = '{}'""".format(sanitize_input(state_name)))
 
     data = cur.fetchall()
 
