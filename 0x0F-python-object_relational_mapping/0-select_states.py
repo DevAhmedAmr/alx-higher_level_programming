8c0de8153200a3d64e72c87115341bb4daa4b703
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-17 20:27:05 +0200

    0. Get all states (automated command)

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3"
 """blank"""
 from MySQLdb import _mysql
+from MySQLdb.constants import FIELD_TYPE
 import sys
-if __name__=="__main__":
-	user=sys.argv[1]
-	password=sys.argv[2]
-	database=sys.argv[3]
-	db=_mysql.connect(host="localhost",user=user,
-					
-					password=password,database=database,port=3306)
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    my_conv = {FIELD_TYPE.LONG: int, FIELD_TYPE.CHAR: str}
+    db = _mysql.connect(host="localhost", user=user,
 
-	db.query("""SELECT * FROM states ORDER BY states.id ASC;""")
+                        password=password, database=database, port=3306, conv=my_conv)
 
-	result=db.store_result()
+    db.query("""SELECT * FROM states ORDER BY states.id ASC;""")
 
-	for row in result:
-		print(row)
+    result = db.use_result()
+    data = result.fetch_row(maxrows=0)
+    for row in data:
+        state_id = row[0]  
+        state_name_bytes = row[1]  
+        state_name = state_name_bytes.decode('utf-8')  
+        print(f"({state_id}, '{state_name}')")
