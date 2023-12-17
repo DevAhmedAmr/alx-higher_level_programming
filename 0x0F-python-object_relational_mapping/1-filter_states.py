72c03e7a9be753aaa02d73089aa779758eebafdf
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-17 21:10:58 +0200

    0. Get all states (automated command)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""blank"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",port=3306, user=user,
+                        password=password, database=database)
+    cur = db.cursor()
+    #"SELECT * FROM products WHERE prod_name LIKE '%A'"
+    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'""")
+
+    data = cur.fetchall()
+    for row in data:
+        print(row)
+    cur.close()
+    db.close()
