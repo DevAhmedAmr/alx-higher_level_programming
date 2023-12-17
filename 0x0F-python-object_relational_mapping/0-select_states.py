1d18bfa03986dd3ebda619797b68f37491442001
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-17 19:58:52 +0200

    0. Get all states  (automated command)

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from MySQLdb import _mysql
+import sys
+if __name__=="__main__":
+	user=sys.argv[1]
+	password=sys.argv[2]
+	database=sys.argv[3]
+	db=_mysql.connect(host="localhost",user=user,
+					
+					password=password,database=database,port=3306)
+	var=db.query("""SHOW states FROM hbtn_0e_0_usa ORDER BY states.id ASC;""")
+	for row in var:
+		print(row)
