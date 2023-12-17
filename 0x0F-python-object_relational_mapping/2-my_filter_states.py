2adbfea287b1f72a77695d08bbe2da583173892d
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-17 21:45:00 +0200

    2. Filter states by user input (automated command)

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,16 +6,18 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name=sys.argv[4]
+    state_name = sys.argv[4]
     db = MySQLdb.connect(host="localhost", port=3306, user=user,
                          password=password, database=database)
     cur = db.cursor()
     # "SELECT * FROM products WHERE prod_name LIKE '%A'"
     cur.execute(
-          """SELECT * FROM states WHERE states.name='{}';""".format(state_name))
+       """SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'""".format(state_name))
 
     data = cur.fetchall()
-    
+
     for row in data:
         print(row)
     cur.close()
