e04e574bf17aad7a509e3b905be72016deb5c329
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-17 20:42:30 +0200

    0. Get all states (automated command)

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", user=user,
-                        password=password, database=database, port=3306)
+    db = MySQLdb.connect(host="localhost",port=3306, user=user,
+                        password=password, database=database)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY states.id ASC;""")
+    cur.execute("""SELECT * FROM states ORDER BY id ASC;""")
 
     data = cur.fetchall()
     for row in data:
