f2593c8a5212f0b45c4a823997af29841fac2452
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-18 00:59:39 +0200

    4. Cities by states (automated command)

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""blank"""
+import MySQLdb
+import sys
+import re
+"""blank"""
+
+if __name__ == "__main__":
+
+    def sanitize_input(input_string):
+        sanitized_string = re.sub(r'[^a-zA-Z0-9\s]', '', input_string)
+        return sanitized_string
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         password=password, database=database)
+    cur = db.cursor()
+    # "SELECT * FROM products WHERE prod_name LIKE '%A'"
+    query = """SELECT cities.id, cities.name AS city_name, states.name AS state_name
+				FROM cities
+				JOIN states ON cities.state_id = states.id
+				ORDER BY cities.id ASC;"""
+    cur.execute(query)
+
+    data = cur.fetchall()
+
+    for row in data:
+        print(row)
+    cur.close()
+    db.close()
