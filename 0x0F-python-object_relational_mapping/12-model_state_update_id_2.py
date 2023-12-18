99e6226dc142f9e110d4a32668b28d0fb9433b11
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-19 00:08:45 +0200

    12. Update a state (automated command)

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,3 +10,20 @@ from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    hostname = "localhost"
+    port = "3306"
+    database_name = sys.argv[3]
+
+    connection_string = (
+        f"mysql://{username}:{password}@{hostname}:{port}/{database_name}"
+    )
+    engine = create_engine(connection_string, echo=False)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
+    session.commit()
