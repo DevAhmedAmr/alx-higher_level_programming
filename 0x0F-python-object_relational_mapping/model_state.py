53acdfde945a7554a490cc62898434f15598ba38
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-18 18:36:24 +0200

    (automated command)

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,12 +2,25 @@
 # Defines a State model.
 # Inherits from SQLAlchemy Base and links to the MySQL table states.
 """doc"""
+from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+import sys
+
 
 Base = declarative_base()
+"""doc1"""
 
-"""doc"""
+username = sys.argv[1]
+password = sys.argv[2]
+hostname = "localhost"  # or the address where your MySQL server is hosted
+port = "3306"  # default MySQL port is 3306
+database_name = sys.argv[3]
+
+connection_string = f"mysql://{username}:{password}@{hostname}:{port}/{database_name}"
+engine = create_engine(connection_string, echo=True)
+
+"""doc2"""
 
 
 class State(Base):
@@ -19,5 +32,5 @@ class State(Base):
     """
 
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
