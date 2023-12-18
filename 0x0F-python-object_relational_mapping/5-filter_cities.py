1d39a3909d6385372c4c0771de4df3342df7385c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-18 02:29:10 +0200

    5. All cities by state (automated command)

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,14 +7,26 @@ import re
 
 if __name__ == "__main__":
 
+    def print_formatted(data: tuple):
+        for i in range(len(data)):
+            for j in range(len(data[i])):
+                print(data[i][j], end="")
+
+                if i < len(data)-1:
+                    print(", ", end="")
+                else:
+                    if j < len(data[i])-1:
+                        print(", ", end="")
+        print("")
+
     def sanitize_input(input_string):
         sanitized_string = re.sub(r'[^a-zA-Z0-9\s]', '', input_string)
         return sanitized_string
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name=sys.argv[4]
-    
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(host="localhost", port=3306, user=user,
                          password=password, database=database)
     cur = db.cursor()
@@ -24,12 +36,9 @@ if __name__ == "__main__":
                 FROM cities ,states
                 where  states.name=%s And cities.state_id = states.id
                 ORDER BY cities.id ASC;"""
-    cur.execute(query,(state_name,))
+    cur.execute(query, (state_name,))
 
     data = cur.fetchall()
-    city_names = ', '.join(row[0] for row in data)
-    print(city_names)
-
-        
+    print_formatted(data)
     cur.close()
     db.close()
