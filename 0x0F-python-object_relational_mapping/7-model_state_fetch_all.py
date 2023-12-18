686c5e7f34b86f20f847ce970e8679f396d324f1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-18 19:59:16 +0200

    7. All states via SQLAlchemy (automated command)

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,18 +5,27 @@
 #
 # <database name>
 """doc"""
-
-import sys
-from sqlalchemy import create_engine
+from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sqlalchemy import create_engine
+import sys
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    hostname = "localhost"  # or the address where your MySQL server is hosted
+    port = "3306"  # default MySQL port is 3306
+    database_name = sys.argv[3]
+
+    connection_string = (
+        f"mysql://{username}:{password}@{hostname}:{port}/{database_name}"
+    )
+    engine = create_engine(connection_string, echo=False)
+
     Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    for i in session.query(State).order_by(State.id):
+        print(f"{i.id}: {i.name}")
