0b3c4634bd6b2a42d47b5cc9a9b9f6f1e5d1b3c1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-18 18:26:44 +0200

    task 6 rework (automated command)

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,22 +1,23 @@
-#!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
-"""doc"""
+from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+import sys
 
 """doc"""
+Base = declarative_base()
+
+username = sys.argv[1]
+password = sys.argv[2]
+hostname = "localhost"  # or the address where your MySQL server is hosted
+port = "3306"  # default MySQL port is 3306
+database_name = sys.argv[3]
+
+# MySQL connection string format: 'mysql://username:password@hostname:port/database_name'
+connection_string = f"mysql://{username}:{password}@{hostname}:{port}/{database_name}"
+engine = create_engine("sqlite:///:memory:", echo=True)
 
 
 class State(Base):
-    """Represents a state for a MySQL database.
-
-    __tablename__ (str): The name of the MySQL table to store States.
-    id (sqlalchemy.Integer): The state's id.
-    name (sqlalchemy.String): The state's name.
-    """
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
