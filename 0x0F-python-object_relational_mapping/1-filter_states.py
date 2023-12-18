95392d9bc92ee3f6b578108d060bf10ba12dc052
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-18 16:22:11 +0200

    (automated command)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,16 +2,19 @@
 """blank"""
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", port=3306, user=user,
-                         password=password, database=database)
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=user, password=password, database=database
+    )
     cur = db.cursor()
     # "SELECT * FROM products WHERE prod_name LIKE '%A'"
     cur.execute(
-        """SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id""")
+        """SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY states.id"""
+    )
 
     data = cur.fetchall()
     for row in data:
