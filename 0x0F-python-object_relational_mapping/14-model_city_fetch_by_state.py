81ea0b30c458fff2c31cdfb8b86627895284f5cf
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-12-19 01:49:34 +0200

    14. Cities in state (automated command)

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-# Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
-# Usage: ./11-model_state_insert.py <mysql username> /
-#                                   <mysql password> /
-#
-"""doc"""
-import sys
+""" prints all City objects from the database"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_city import City
 from model_state import State
+import sys
+
+""" prints all City objects from the database"""
+
 
 if __name__ == "__main__":
+    """prints all City objects from the database"""
     username = sys.argv[1]
     password = sys.argv[2]
-    hostname = "localhost"
-    port = "3306"
     database_name = sys.argv[3]
 
+    hostname = "localhost"
+    port = 3306
     connection_string = (
         f"mysql://{username}:{password}@{hostname}:{port}/{database_name}"
     )
     engine = create_engine(connection_string, echo=False)
-
-    Session = sessionmaker(bind=engine)
-
+    Session = sessionmaker()
+    Session.configure(bind=engine)
     session = Session()
 
-    new_entry = State(name="Louisiana")
+    result = session.query(City).filter(City.state_id == State.id).order_by(City.id)
 
-    session.add(new_entry)
-    session.commit()
-    print(new_entry.id)
+    for r in result:
+        state_name = session.query(State).filter(State.id == (r.state_id)).first().name
+        print(f"{state_name}: ({r.id}) {r.name}")
