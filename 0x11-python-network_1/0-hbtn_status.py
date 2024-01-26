7f85b99a7f52f8136ede2b1f12d0396da37ea1d3
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-27 01:22:28 +0200

    0. What's my status? #0  (automated command)

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,9 @@ import urllib.request
 
 if __name__ == "__main__":
     "script that fetches https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+
+    link = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(link) as response:
         html = response.read()
         print("Body response:")
         print(f"\t- type: {type(html)}")
