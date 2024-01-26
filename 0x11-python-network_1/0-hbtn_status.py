e2c68ace784da9ba35ba2b4f8b52be771f8de4a4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-27 01:17:27 +0200

    0. What's my status? #0  (automated command)

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,10 @@
 import urllib.request
 "urllib.request"
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-   html = response.read()
-print("Body response:")
-print(f"\t- type: {type(html)}")
-print(f"\t- content: {html}")
-print(f"\t- utf8 content: {html.decode('utf-8')}")
+if __name__ == "__main__":
+	with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+		html = response.read()
+		print("Body response:")
+		print(f"\t- type: {type(html)}")
+		print(f"\t- content: {html}")
+		print(f"\t- utf8 content: {html.decode('utf-8')}")
