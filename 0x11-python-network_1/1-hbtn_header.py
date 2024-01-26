98c315914abbd1623dac544d794bb1869b43cacb
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-27 01:40:00 +0200

    1. Response header value #0 (automated command)

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"urllib.request"
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    "script that fetches X-Request-Id var from the header of the response"
+    link = sys.argv[1]
+
+    with urllib.request.urlopen(link) as response:
+        header_info = response.info()
+        print(header_info["X-Request-Id"])
