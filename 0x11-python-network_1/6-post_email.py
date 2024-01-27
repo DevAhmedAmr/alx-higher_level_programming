73bae41a47dd4bc1888ea2c7788c0da7fa6f7a01
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-28 00:17:25 +0200

    Python script that fetches https://alx-intranet.hbtn.io/status (automated command)

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" urlLib"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    """Python script that takes in a URL and an email address, sends a
+    POST request to the passed URL with the email as a parameter, and finally
+        displays the body of the response."""
+    url = sys.argv[1]
+    email = sys.argv[2]
+    value = {"email": email}
+    r = requests.post(url, data=value)
+    print(r.text)
