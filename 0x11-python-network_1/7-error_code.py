620a265e583ffe29e03e908230853ff43c2e6cc1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-28 00:47:41 +0200

    (automated command)

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     """Python script that takes in a URL, sends a request to
     the URL and displays the body of the response."""
     url = sys.argv[1]
-    r = requests.post(url)
+    r = requests.get(url)
 
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
