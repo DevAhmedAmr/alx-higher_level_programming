7ebac8f4dc1982b7d167e7f56a8b97ae0c9f4de6
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-27 23:47:23 +0200

    Python script that fetches https://alx-intranet.hbtn.io/status (automated command)

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,9 +4,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    "Python script that fetches https://alx-intranet.hbtn.io/status"
+    """Python script that takes in a URL, sends a request to the URL and
+    displays the value of the variable X-Request-Id in the response header"""
     url = sys.argv[1]
     r = requests.get(url)
-    print(r.headers)
-    print(f"\t- type: {type(r.text)}")
-    print(f"\t- content: {r.text}")
+    print(r.headers["X-Request-Id"])
