541eac3710f72670843acdcec6de65760da535b4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-27 23:46:01 +0200

    Python script that fetches https://alx-intranet.hbtn.io/status (automated command)

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" urlLib"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    "Python script that fetches https://alx-intranet.hbtn.io/status"
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers)
+    print(f"\t- type: {type(r.text)}")
+    print(f"\t- content: {r.text}")
