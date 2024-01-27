8c0fc43863af13f889eee4c9402b23f9a2b2feb1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-27 23:28:20 +0200

    3. Error code #0  (automated command)

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,10 +9,9 @@ if __name__ == "__main__":
     """python script that takes in a URL,
             sends a request to the URL and displays the
     body of the response (decoded in utf-8)."""
-
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.URLError as e:
-        print("Error code: ", e.reason[0])
+        print("Error code: {}".format(e.reason[0]))
