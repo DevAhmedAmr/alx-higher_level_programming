983dad007a99971efb145fb3145b7a4580ad62df
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-28 00:24:13 +0200

    7. Error code #1 (automated command)

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" urlLib"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    """Python script that takes in a URL, sends a request to
+    the URL and displays the body of the response."""
+    url = sys.argv[1]
+    r = requests.post(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+        exit(1)
+    print(r.text)
