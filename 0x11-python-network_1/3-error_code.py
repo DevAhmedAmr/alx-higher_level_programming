741fc24e39f1b40a5189adf91e9e8b59ed97c7b1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-01-27 23:21:30 +0200

    3. Error code #0  (automated command)

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,6 +4,10 @@ import urllib.error
 import sys
 
 if __name__ == "__main__":
+    """python script that takes in a URL,
+            sends a request to the URL and displays the
+    body of the response (decoded in utf-8)."""
+
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
